9e2123f5e23492b2df0a56c62a9c4acfadad1e36
Asha Islam <ashaislam31@gmail.com>
2023-01-22 00:07:23 -0500

    creating the sign up page

--- app/src/main/java/com/example/mobileapp_finances/SignUp.java
+++ app/src/main/java/com/example/mobileapp_finances/SignUp.java
@@ -3,12 +3,71 @@ package com.example.mobileapp_finances;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
+
+    //attributes for the sign-up page
+    EditText firstName;
+    EditText lastName;
+    EditText address;
+    EditText email;
+    Button register;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+        address = findViewById(R.id.address);
+        email = findViewById(R.id.email);
+        register = findViewById(R.id.register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkDataEntered();
+            }
+        });
+
     }
+
+    //checking if the first name and the last name are valid
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+
+    //checking if the email is valid
+    boolean isEmail(EditText text) {
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+
+    void checkDataEntered()
+    {
+        if (isEmpty(firstName)) {
+            Toast t = Toast.makeText(this, "You must enter first name to register!", Toast.LENGTH_SHORT);
+            t.show();
+        }
+
+        if (isEmpty(lastName)) {
+            lastName.setError("Last name is required!");
+        }
+
+        if (isEmail(email) == false) {
+            email.setError("Enter valid email!");
+        }
+
+    }
+
 }
