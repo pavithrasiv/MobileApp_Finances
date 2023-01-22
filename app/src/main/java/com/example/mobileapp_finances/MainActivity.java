5a78bf00a12f4b4a1c522d212d8623b4af5e0c42
Asha Islam <ashaislam31@gmail.com>
2023-01-22 03:02:20 -0500

    creation of the taskbar

--- app/src/main/java/com/example/mobileapp_finances/MainActivity.java
+++ app/src/main/java/com/example/mobileapp_finances/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.mobileapp_finances;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar=findViewById(R.id.myToolBar);
+
+        setSupportActionBar(toolbar);
     }
 }
